e3bd0a7d046eab6b6548d447d05796e6671c0996
Austin Techlin <500199737@fvtc.edu>
2024-02-10 09:55:37 -0600

    Added welcome div

--- site.js
+++ site.js
@@ -1,1 +1,11 @@
-//console.log("Hello World!")
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const welcome = document.querySelector('#welcome')
+
+if (isMorning) welcome.textContent = "Good Morning!"
+if (isAfternoon) welcome.textContent = "Good Afternoon!"
+if (isEvening) welcome.textContent = "Good Evening!"
